2bcd8720ee1f758389742a8b108d5bd06c96ed73
Vid <vid@preskok.net>
2021-01-18 10:30:33 +0100

    Added ability to add allergies and remove allergies, added responsives

--- src/pages/add/addPatient.vue
+++ src/pages/add/addPatient.vue
@@ -1,16 +1,55 @@
 <template>
   <v-form>
-    <v-text-field placeholder="Ime"></v-text-field>
-    <v-text-field placeholder="Priimek"></v-text-field>
-    <v-text-field placeholder="Datum rojstva"></v-text-field>
-    <v-text-field placeholder="Tel. številka"></v-text-field>
-    <v-text-field placeholder="Kraj rojstva"></v-text-field>
-    <v-text-field placeholder="E-naslov"></v-text-field>
-    <v-text-field placeholder="Trenutno prebivališče"></v-text-field>
+    <v-row>
+      <v-col cols="12" xl="6" lg="6">
+        <v-text-field
+            placeholder="Ime"
+        >
+        </v-text-field>
+      </v-col>
+      <v-col cols="12" xl="6" lg="6">
+        <v-text-field placeholder="Priimek">
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" xl="6" lg="6">
+        <v-text-field placeholder="Datum rojstva">
+        </v-text-field>
+      </v-col>
+      <v-col cols="12" xl="6" lg="6">
+        <v-text-field placeholder="Kraj rojstva">
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-text-field placeholder="Tel. številka">
+    </v-text-field>
+
+    <v-text-field placeholder="E-naslov">
+    </v-text-field>
+
+    <v-text-field placeholder="Trenutno prebivališče">
+    </v-text-field>
+
     <v-select
         :items="bloodTypes"
         label="Tip krvi"
     ></v-select>
+
+    <v-text-field placeholder="Vpiši alergijo.." v-model="allergieInput" @keyup.enter="addAllergie">
+    </v-text-field>
+
+    <v-list>
+      <v-list-item v-for="(value, index) in allergies" :key="index">
+        <v-list-item-icon>
+          <v-icon>mdi-menu-right</v-icon>
+        </v-list-item-icon>
+        {{ value }}
+        <v-spacer>
+        </v-spacer>
+        <v-btn icon color="red" @click="deleteAllergie(value)"><v-icon>mdi-delete</v-icon></v-btn>
+      </v-list-item>
+    </v-list>
 
     <v-btn
         block
@@ -26,6 +65,8 @@ export default {
   name: "addPatient.vue",
   data() {
     return {
+      allergieInput: null,
+      allergies: ['Penicillin', 'Orehi'],
       bloodTypes: [
         "O+",
         "O-",
@@ -36,6 +77,14 @@ export default {
         "AB+",
         "AB-"
       ]
+    }
+  },
+  methods:{
+    addAllergie(){
+      this.allergies.push(this.allergieInput)
+    },
+    deleteAllergie(e){
+      this.allergies.pop(e)
     }
   }
 }
